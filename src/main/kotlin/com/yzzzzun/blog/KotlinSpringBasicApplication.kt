5652b18578e6227651bbd68be889816e3df6e15e
yzzzzun <yzzzzun@gmail.com>
2023-01-10 02:21:18 +0900

    feat: kotlin + spring 프로젝트 생성

---
+++ src/main/kotlin/com/yzzzzun/blog/KotlinSpringBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.yzzzzun.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBasicApplication>(*args)
+}
---
+++ src/test/kotlin/com/yzzzzun/blog/KotlinSpringBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yzzzzun.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBasicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
